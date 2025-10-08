1136727c96eed08dd1809ac427d0eb5a0b4d75e6
tournesol <ttournesol2000@yahoo.fr>
2025-10-08 11:38:50 +0100

    Add componentTempalte

---
+++ grc_templates/uitemplate/TemplateName.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'TemplateName.css'
+
+const TemplateName = (props) => { 
+  return (
+    <div className="TemplateName" data-testid="TemplateName">
+      TemplateName Component
+    </div>
+  )
+};
+
+export default TemplateName;
---
+++ src/components/ui/Button/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'Button.css'
+
+const Button = (props) => { 
+  return (
+    <div className="Button" data-testid="Button">
+      Button Component
+    </div>
+  )
+};
+
+export default Button;
